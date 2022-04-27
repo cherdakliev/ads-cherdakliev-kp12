35ef97cb65195ab497286a5f9e1566afce3964b0
cherdakliev <cherdakliev.kyrylo@lll.kpi.ua>
2022-04-27 13:33:07 +0300

    Lab_06 finished

---
+++ Lab_06/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace asd_lab_06
+{
+    class Program
+    {
+        static void Main()
+        {
+            do
+            {
+                WriteLine();
+                Write("Введiть 'example' для показу контрольного прикладу. Введiть 'user' для введення з клавiатури: "); // Користувацьке меню
+                string s = ReadLine();
+                if (s.ToLower() == "example")
+                {
+                    LinearQueue q = new LinearQueue(10);
+                    string[] arr1 = new string[10] { "Acn65", "76nCB", "98KIC8", "iol", "j7k", "gg8", "67hh", "fff7jk8", "87jh", "fdddl"};
+                    string[] arr2 = new string[5] { "GDF4", "ddf5", "123dOL", "dc11", "UIO6"};
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("Червноим кольором позначаються голова та хвiст черги "); //КОНТРОЛЬНИЙ ПРИКЛАД
+                    ResetColor();
+                    WriteLine("Контрольний приклад: ");
+                    WriteLine("Розмір черги: 10 ");
+                    WriteLine();
+                    q.Print();
+                    WriteLine();
+
+                    for (int i = 0; i < arr1.Length; i++)
+                    {
+                        string m = "";
+                        if (arr1[i] == "j7k" || arr1[i] == "fff7jk8")
+                        {
+                            m = "(ЗА УМОВОЮ ЗАВДАННЯ ЦЕЙ ЕЛЕМЕНТ НЕ БУДЕ ДОДАНИЙ, А ПЕРШИЙ ЕЛЕМЕНТ ЧЕРГИ БУДЕ ВИЛУЧЕНИЙ)";
+                            WriteLine("Додаэмо елемент: " + arr1[i] + m);
+                            AddToQueue(arr1[i], q, ref i);
+                            i = i + 2;
+                        }
+                        else
+                        {
+                            WriteLine("Додаэмо елемент: " + arr1[i] + m);
+                            AddToQueue(arr1[i], q, ref i);
+                        }
+                        
+                    }
+
+                    for (int i = 0; i < arr2.Length; i++)
+                    {
+                        WriteLine("Додаэмо елемент: " + arr2[i]);
+                        AddToQueue(arr2[i], q, ref i);
+                    }
+                }
+                else if (s.ToLower() == "user")
+                {
+                    Write("Введіть довжину черги: ");
+                    int size = Convert.ToInt32(ReadLine());
+                    LinearQueue q = new LinearQueue(size);
+                    int k = 0;
+                    while (k != size)
+                    {
+                        Write("Введіть елемент у чергу: ");
+                        string item = ReadLine();
+                        WriteLine("Додаэмо елемент: " + item);
+                        AddToQueue(item, q,  ref k);
+                        k++;
+                    }
+                    WriteLine();
+                    q.Enqueue("3");
+                }
+                else
+                {
+                    WriteLine("Помилка!");
+                }
+                Write("Do you want to continue (Y/N)? ");
+            } while (ReadKey().Key != ConsoleKey.N);
+            
+ 
+        }
+
+        static void AddToQueue(string data, LinearQueue q, ref int k)
+        {
+            char[] a = new char[100];
+            a = data.ToCharArray();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+
+                if (i != 0 && i != a.Length - 1)
+                {
+                    bool num = ((Convert.ToInt32(a[i]) >= 48) && (Convert.ToInt32(a[i]) <= 57));//перевірка чи елемети підхоядть до умови вилучення
+                    if (num)
+                    {
+                        bool letter1 = ((Convert.ToInt32(a[i - 1]) >= 65) && (Convert.ToInt32(a[i - 1]) <= 122));
+                        bool letter2 = ((Convert.ToInt32(a[i + 1]) >= 65) && (Convert.ToInt32(a[i + 1]) <= 122));
+                        if (num && letter1 && letter2)
+                        {
+                            q.Dequeue();
+                            k = k - 2;
+                            break;
+                        }
+                    }
+                }
+                else if (i == a.Length - 1)
+                {
+                    q.Enqueue(data);
+                    break;
+                }
+
+            }
+        }
+    }
+
+
+}
---
+++ Lab_06/Queue.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asd_lab_06
+{
+    public class LinearQueue
+    {
+        private int front, rear;
+        private string[] array;
+
+        public LinearQueue(int size)
+        {
+            this.front = this.rear = -1;
+            this.array = new string[size];
+        }
+
+        public void Enqueue(string data) //додавання елементу в чергу
+        {
+            
+            if (isEmpty())
+            {
+                front++;
+            }
+            if (isFull())
+            {
+                Console.WriteLine("Черга переповнена. Починається процес очищення черги...");
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    Dequeue();
+                }
+                Console.WriteLine("Черга пуста");
+                rear = front = -1;
+            }
+            else
+            {
+                rear++;
+                array[rear] = data;
+                Print();
+                Console.WriteLine();
+            }
+        }
+
+       
+        public void Dequeue() //Вилучення елементу
+        {
+            Console.ResetColor();
+            string el = array[front];
+            array[front] = "";
+            if (isEmpty())
+            {
+                throw new Exception("Черга пуста");
+            }
+            else if (front == rear)
+                front = rear = -1;
+            else
+            {
+                rear--;
+            }          
+            for (int k = 0; k < array.Length - 1; k++)
+            {
+                array[k] = array[k + 1];
+               
+            }
+            array[array.Length - 1] = ", ";
+            Console.WriteLine("Вилучений елемент: " + el);
+            Console.WriteLine();
+
+        }
+
+        private bool isFull() 
+        {
+            return rear == (array.Length - 1);
+        }
+        private bool isEmpty()
+        {
+            return (front == -1) && (rear == -1);
+        }
+
+        public void Print()
+        {
+            Console.Write("Черга: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ((i == front) || (i == rear))
+                    Console.ForegroundColor = ConsoleColor.Red;
+                else
+                    Console.ResetColor();
+                Console.Write(array[i] + ", ");
+
+            }
+        }
+    }
+}
